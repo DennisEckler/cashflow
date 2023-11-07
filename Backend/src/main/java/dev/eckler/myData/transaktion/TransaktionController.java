ba2b7dcaa668d196da54ba37f1ef33c651d22e6e
Dennis.Eckler <eckler.d95@gmail.com>
2023-11-07 21:24:40 +0100

    created class with endpoints from jpa

--- Backend/src/main/java/dev/eckler/myData/transaktion/TransaktionController.java
+++ Backend/src/main/java/dev/eckler/myData/transaktion/TransaktionController.java
@@ -1,14 +1,32 @@
 package dev.eckler.myData.transaktion;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.sql.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class TransaktionController {
 
-  @GetMapping("/transaktion")
-  public Transaktion getTransaktion() {
-    Transaktion transaktion = new Transaktion("Description", 4.32f, "20221001");
-    return transaktion;
+  private final TransaktionRepository transaktionRepository;
+
+  public TransaktionController(TransaktionRepository transaktionRepository) {
+    this.transaktionRepository = transaktionRepository;
   }
+
+  @GetMapping("/transaktion")
+  public Iterable<Transaktion> getTransaktions() {
+    Date date = new Date(20231010);
+    this.transaktionRepository.save(
+        new Transaktion(date, "agent", "some bookin done", "the purpose", 2.23f));
+    return transaktionRepository.findAll();
+  }
+
+  @PostMapping("/add")
+  public Transaktion addTransaktion(@RequestBody Transaktion request) {
+    return this.transaktionRepository.save(request);
+  }
+
 }
---
+++ Backend/src/main/java/dev/eckler/myData/transaktion/TransaktionRepository.java
@@ -0,0 +1,6 @@
+package dev.eckler.myData.transaktion;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TransaktionRepository extends CrudRepository<Transaktion, Long> {
+}
