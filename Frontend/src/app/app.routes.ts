32dc29457d428495ed6d1a87e215237cd7389af0
Dennis.Eckler <eckler.d95@gmail.com>
2023-12-02 12:13:00 +0100

    removed RouterModules and implemented config file

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { UpdateListComponent } from './components/update-list/update-list.component';
+import { ShowTransaktionsComponent } from './components/show-transaktions/show-transaktions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'file-upload', title: 'FileUpload', component: FileUploadComponent },
+  { path: 'update-list', component: UpdateListComponent },
+  { path: 'show-transaktions', component: ShowTransaktionsComponent },
+];
--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,7 +1,18 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  withInterceptorsFromDi,
+  provideHttpClient,
+} from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, FormsModule, NgOptimizedImage),
+    provideHttpClient(withInterceptorsFromDi()),
+    appConfig.providers,
+  ],
+}).catch((err) => console.error(err));
