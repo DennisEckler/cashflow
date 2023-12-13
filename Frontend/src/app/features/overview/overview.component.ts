315c85151ab3adebf31280da70f1f4a107062305
Dennis.Eckler <eckler.d95@gmail.com>
2023-12-13 20:41:33 +0100

    created component

--- Frontend/src/app/features/overview/overview.component.ts
+++ Frontend/src/app/features/overview/overview.component.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { OverviewService } from './overview.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { OverviewRow } from 'src/app/core/model/overviewRow';
+import { OverviewRowComponent } from 'src/app/shared/overview-row/overview-row.component';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, OverviewRowComponent],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
 })
 export class OverviewComponent implements OnInit {
   overviewSummary: OverviewRow[] = [];
+
   constructor(private overviewService: OverviewService) {}
   ngOnInit() {
     this.overviewService.getOverview().subscribe({
