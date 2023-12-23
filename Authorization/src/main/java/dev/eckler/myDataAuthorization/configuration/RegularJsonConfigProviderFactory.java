8e35e603f500fb88a92bd142fad4e69d2f6e1e46
Dennis.Eckler <eckler.d95@gmail.com>
2023-12-23 10:31:16 +0100

    removed embedded auth

--- Authorization/src/main/java/dev/eckler/myDataAuthorization/configuration/RegularJsonConfigProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package dev.eckler.myDataAuthorization.configuration;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-
-}
