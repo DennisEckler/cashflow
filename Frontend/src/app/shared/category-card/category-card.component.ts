27cb2295e573486aa97f09fe832f2649b6d24b25
Dennis.Eckler <eckler.d95@gmail.com>
2024-02-14 20:40:11 +0100

    wip add logic to create an input from frontend

--- Frontend/src/app/shared/category-card/category-card.component.ts
+++ Frontend/src/app/shared/category-card/category-card.component.ts
@@ -4,16 +4,18 @@ import { Category } from 'src/app/core/model/category';
 import { Input } from '@angular/core';
 import { IdentifierChipComponent } from '../identifier-chip/identifier-chip.component';
 import { Identifier } from 'src/app/core/model/identifier';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-category-card',
   standalone: true,
-  imports: [CommonModule, IdentifierChipComponent],
+  imports: [CommonModule, IdentifierChipComponent, FormsModule],
   templateUrl: './category-card.component.html',
   styleUrl: './category-card.component.scss',
 })
 export class CategoryCardComponent {
   @Input() category?: Category;
+  identifierInput: string = '';
 
   deleteIdentifier(deleteIdentifer: Identifier) {
     if (this.category) {
@@ -21,5 +23,14 @@ export class CategoryCardComponent {
         (identifer) => identifer.identifierID !== deleteIdentifer.identifierID
       );
     }
+  }
+
+  addIdentifier() {
+    if (this.identifierInput !== '') {
+    }
+  }
+
+  onChange(event: any) {
+    this.identifierInput = event.target.value;
   }
 }
