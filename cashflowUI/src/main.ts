eef9155f02acd4cd0507871c2faa3941fcd99282
Dennis Eckler <devops.eckler@proton.me>
2024-12-21 10:48:24 +0100

    migrate angular 17 -> 18

--- cashflowUI/src/main.ts
+++ cashflowUI/src/main.ts
@@ -2,10 +2,7 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { NgOptimizedImage } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {
-  withInterceptorsFromDi,
-  provideHttpClient,
-} from '@angular/common/http';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { provideAnimations } from '@angular/platform-browser/animations';
