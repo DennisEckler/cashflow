c0f880f5cc344245068536fa0eca0bdf2b0a6f9b
Dennis Eckler <devops.eckler@proton.me>
2024-12-28 16:35:28 +0100

    test

--- cashflowUI/src/environments/environment.ts
+++ cashflowUI/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   cashflowUrl: '/api',
-  issuer: 'http://192.168.2.65:8080/realms/cashflow_realm',
+  issuer: 'http://192.168.2.65:9000/realms/cashflow_realm',
 };
