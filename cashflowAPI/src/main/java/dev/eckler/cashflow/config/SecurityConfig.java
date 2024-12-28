e96c48cc267d5edb56d0e2f2687e1c54360375c3
Dennis Eckler <devops.eckler@proton.me>
2024-12-28 17:29:03 +0100

    add always success issuer

--- cashflowAPI/src/main/java/dev/eckler/cashflow/config/SecurityConfig.java
+++ cashflowAPI/src/main/java/dev/eckler/cashflow/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package dev.eckler.cashflow.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,6 +28,16 @@ public class SecurityConfig {
   @Bean
   public WebSecurityCustomizer webSecurityCustomizer() {
     return (web) -> web.ignoring().requestMatchers("/h2/**");
+  }
+
+  @Bean
+  @Profile("prod")
+  public JwtDecoder jwtDecoder(){
+    NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation("http://cashflow-auth:9000/realms/cashflow_realm");
+    jwtDecoder.setJwtValidator(jwt ->{
+      return OAuth2TokenValidatorResult.success();
+    });
+    return jwtDecoder;
   }
 
 }
