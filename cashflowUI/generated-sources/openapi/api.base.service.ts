26c66135fa65a1c30387ddacf7703ae87612e1e7
Dennis Eckler <devops.eckler@proton.me>
2026-01-20 21:02:06 +0100

    changed base service url

--- cashflowUI/generated-sources/openapi/api.base.service.ts
+++ cashflowUI/generated-sources/openapi/api.base.service.ts
@@ -12,7 +12,7 @@ import { CustomHttpParameterCodec } from './encoder';
 import { Configuration } from './configuration';
 
 export class BaseService {
-    protected basePath = 'http://localhost:8080/v1/api';
+    protected basePath = 'http://eckler:8080/v1/api';
     public defaultHeaders = new HttpHeaders();
     public configuration: Configuration;
     public encoder: HttpParameterCodec;
