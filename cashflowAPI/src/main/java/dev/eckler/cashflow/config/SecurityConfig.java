4853be1a473f17e3ff128cd0358455cee76081f2
Dennis Eckler <devops.eckler@proton.me>
2026-02-28 22:06:03 +0100

    test

--- cashflowAPI/src/main/java/dev/eckler/cashflow/config/SecurityConfig.java
+++ cashflowAPI/src/main/java/dev/eckler/cashflow/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -53,6 +54,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Profile("prod")
     public JwtDecoder jwtDecoder() {
         // Fetch JWKS keys via internal container network (no Traefik needed)
         NimbusJwtDecoder decoder = NimbusJwtDecoder
